48c65cc3d750743ddec5cb2012d8fc341f4eccd6
Diya Thakur <diya.229302194@muj.manipal.edu>
2025-03-14 08:35:26 +0530

    add css config files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Scans these files for Tailwind classes
+    theme: {
+      extend: {}, // Customize your theme here if needed
+    },
+    plugins: [], // Add Tailwind plugins here if needed
+  }
+  
